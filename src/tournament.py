fca79e77a1c0bdafe0105787466df9f96fb4ecce
Winter Mason <winteram@gmail.com>
2011-11-30 11:20:11 -0500

    Added more features

--- src/tournament.py
+++ src/tournament.py
@@ -4,6 +4,22 @@
 # load required packages
 import random
 import sys, os
+import logging
+from scipy.stats import bernoulli,poisson,norm,expon,uniform
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+logger=logging.getLogger('SLSTournament')
+
+# Each strategy must have two functions, "makeMove" and "observe"
+# "makeMove" takes as input: 
+#    canPlayRefine, canChooseModel, currentDeme, roundsAlive, repertoire, history
+# "makeMove"  must return: 
+#    move, action, currentRep
+# "observeWho" takes as input:
+#    modelInfo, a list of dictionaries, one for each potential model, with the info:
+#       age, total payoff, times observed, # offspring
+# "observeWho" must return:
+#    models, an ordered list of indices corresponding to modelInfo
 
 # Load agents from agent directory
 agent_dir = 'agents/'
@@ -14,7 +30,11 @@ for file in agentfiles:
     if fname[1] == "py":
         import fname[0]
         strategies[] = fname[0]
+##TODO: catch if no directory or files
 
+
+
+##TODO: Allow importing of parameters with input file
 # Initialize parameters in model
 canPlayRefine = True  # if refine move is available
 canChooseModel = True # if observe_who is an option
@@ -27,14 +47,28 @@ nact = 100 # number of possible actions
 
 lambd = 0.8 # 1 / mean of exponential distribution
 pchg = 0.05 # probability of environment changing
-pmut = 0.04 # probability of mutation
+pmut = 0.02 # probability of mutation
 pmig = 0.03 # probability of migration (if spatial extension is enabled)
+pdie = 0.02 # probability of dying
 
 copy_error = 0.01
 sigma = 0.2
 
 # structure for a new agent
-newagent = {"strategy":initStrategy, "rep":{}, "lastmove":-99, "history":{"historyRounds":[],"historyMoves":[],"historyActs":[],"historyPayoffs":[], "historyDemes":[]}, "roundsAlive": 0 , "currentDeme": 0, "pointsEarned": 0}
+newagent = {"strategy":initStrategy, 
+            "rep":{}, 
+            "lastMove": -1,
+            "history": {"historyRounds":[],
+                        "historyMoves":[],
+                        "historyActs":[],
+                        "historyPayoffs":[], 
+                        "historyDemes":[] }, 
+            "born": 0,
+            "roundsAlive": 0, 
+            "nObserved":0,
+            "currentDeme": 0, 
+            "pointsEarned": 0,
+            "nOffspring":0}
 
 # Initialize structures in model
 payoff = [] # payoff landscape
@@ -42,57 +76,89 @@ payoff[0] = [round(2*random.expovariate(lamd)**2) for x in range(nact)]
 payoff[1] = [round(2*random.expovariate(lamd)**2) for x in range(nact)]
 payoff[2] = [round(2*random.expovariate(lamd)**2) for x in range(nact)]
 
+
+# Initialize stats
+statsDict = {"aliveAgents":0,
+             "innovate":0,
+             "observe":0,
+             "exploit":0,
+             "refine":0,
+             "totalPayoffs":0,
+             "lifespans":[]}
+roundStats = {}
+for i in strategies:
+    roundStats[strategies[i]] = statsDict
+gameStats = []
+
+# Initialize agents
 if runIn:
     initStrategy = random.choice(strategies)
     for i in range(N):
-        agentList[] = newagent # list of all agents
-        aliveAgents[] = newagent  # list of currently playing agents
+        Agents[] = newagent  # list of all agents
+        aliveAgents[] = i # list of currently playing agents
 else:
     for i in range(N):
         initStrategy = random.choice(strategies)
-        agentList[] =  # list of all agents
-        aliveAgents[] = newagent  # list of currently playing agents
+        Agents[] = newagent  # list of all agents
+        aliveAgents[] = i # list of currently playing agents
 
 
 # Loop through each generation
 for generation in range(ngen): 
-    # Loop through each agent
-    for i in range(N):
-	# get strategy from agent (call function named "strategy")
-	move, currentRep, observe_who = globals()[aliveAgents[i]["strategy"]](aliveAgents[i]["roundsAlive"],aliveAgents[i]["rep"],aliveAgents[i]["history"])
-	# Do move:
-        # if innovate, pick unknown behavior
-        if move == -1:
-            unknownActs = set(payoff[aliveAgents[i]["currentDeme"]]) - set(aliveAgents[i]["rep"].keys())
-            if len(unknownActs) == 0:
-                act = 0
-                payoff = 0
-            else:
-                act = random.choice(unknownActs)
-                payoff = payoff[aliveAgents[i]["currentDeme"]][act]
-                aliveAgents[i]["rep"][act] = payoff
-            aliveAgents[i]["lastmove"] = -1
-            aliveAgents[i]["history"]["historyRounds"][] = generation
-            aliveAgents[i]["history"]["historyMoves"][] = -1
-            aliveAgents[i]["history"]["historyActs"][] = act
-            aliveAgents[i]["history"]["historyPayoffs"][] = payoff
-            aliveAgents[i]["history"]["historyDemes"][] = aliveAgents[i]["currentDeme"]
+    # initialize stats for this round
+    gameStats[] = roundStats
 
-        # if observe:
-        elif move == 0:
+    # create this round's data to pass to observeWho function
+    if canChooseModel:
+        modelInfo = []
+        for i in aliveAgents:
+            modelInfo[] = {"age":Agents[i]["roundsAlive"],
+                           "totalPoints":Agents[i]["pointsEarned"],
+                           "nObserved":Agents[i]["nObserved"],
+                           "nOffspring":Agents[i]["nOffspring"]}
+
+    # Loop through each agent
+    for i in aliveAgents:
+	# get strategy from agent (call function named "strategy")
+	agentAction = globals()[Agents[i]["strategy"]].makeMove(canPlayRefine, canChooseModel,Agents[i]["currentDeme"], Agents[i]["roundsAlive"],Agents[i]["rep"],Agents[i]["history"])
+
+        # Catch bad strategy output
+        if move not in agentAction.keys():
+            logger.error("Agent " + i + " did not return a move")
+            continue
+
+	# Do move:
+        # INNOVATE
+        if agentAction["move"] == "INNOVATE":
+            gameStats[generation][Agents[i]["strategy"]]["innovate"] += 1
+            unknownActs = set(payoff[Agents[i]["currentDeme"]].keys()) - set(Agents[i]["rep"].keys())
+            if len(unknownActs) > 0:
+                act = random.choice(unknownActs)
+                payoff = payoff[Agents[i]["currentDeme"]][act]
+                Agents[i]["rep"][act] = payoff
+
+        # OBSERVE:
+        elif agentAction["move"] == "OBSERVE":
+            gameStats[generation][Agents[i]["strategy"]]["observe"] += 1
+            payoff = 0
             exploiters = []
             observed_acts = {}
-            # if there is anyone to observe
-            for j in len(aliveAgents):
-                if j != i and aliveAgents[j]["lastmove"]>0:
+
+            # can only observe agents in same deme who exploited on last round
+            for j in len(Agents):
+                if j != i and Agents[j]["lastMove"]=="EXPLOIT" and Agents[i]["currentDeme"]==Agents[j]["currentDeme"]:
                     exploiters.append(j)
 
             models = []
             # if observe_who extension is enabled
             if canChooseModel:
-                for model in range(min(len(exploiters),nObserve)):
-                    if observe_who[model] in exploiters:
-                        models[model] = observe_who[model]
+                # get ordered list of models (call observeWho function)
+                rankedModels = globals()[Agents[i]["strategy"]].observeWho(modelInfo)
+                #TODO: handle no observeWho function
+                #TODO: handle ranked models length < exploiters length
+                for j in range(min(len(rankedModels),nObserve)):
+                    if rankedModels[j] in exploiters:
+                        models[j] = rankedModels[j]
             # else
             else:
                 # select random group to observe from
@@ -103,41 +169,64 @@ for generation in range(ngen):
                 
             # create dictionary of actions & payoffs to add to repertoire
             for model in models:
-                for action,payoff in aliveAgents[model]["rep"]:
-                    if action not in aliveAgents[i]["rep"]:
-                        observed_acts[action] = payoff + random.gauss(0,sigma)
+                Agents[model]["nObserved"] += 1
+                for action,payoff in Agents[model]["rep"]:
+                    if action not in Agents[i]["rep"]:
+                        observed_acts[action] = poisson.rvs(sigma,payoff)
             
             # add copy errors
             for action in observed_acts:
-                if random.rand() < copy_error: # SYNTAXERROR
+                if random.random() < copy_error:
                     observed_acts[action] = 0
 
             # add to repertoire
-            aliveAgents[i]["rep"].extend(observed_acts)  # SYNTAXERROR
+            Agents[i]["rep"].extend(observed_acts) 
 			
-        # if exploit, add payoff to agent's total
-        elif move>0 and move < 100:
-            aliveAgents[i]["pointsEarned"] += payoff[aliveAgents[i]["currentDeme"]][move]
+        # EXPLOIT
+        elif agentAction["move"] == "EXPLOIT":
+            gameStats[generation][Agents[i]["strategy"]]["exploit"] += 1
+            if agentAction["action"]>0 and agentAction["action"] < 100:
+            if agentAction["action"] in agentAction["rep"].keys():
+                payoff = payoff[Agents[i]["currentDeme"]][agentAction["action"]] + 1
+                Agents[i]["pointsEarned"] += payoff
+                Agents[i]["rep"][agentAction["action"]] = payoff
+                gameStats[generation][Agents[i]["strategy"]]["totalPayoffs"] += payoff
+            else:
+                logger.error("Agent " + i + " exploited action not in its repertoire")
         
-        # if refine
-        elif move==-2 and canPlayRefine:
-            
+        # REFINE
+        elif agentAction["move"] == "REFINE" and canPlayRefine:
+            gameStats[generation][Agents[i]["strategy"]]["refine"] += 1
+            if agentAction["action"] in agentAction["rep"].keys():
+                payoff = payoff[Agents[i]["currentDeme"]][agentAction["action"]] + 1
+                agentAction["rep"][agentAction["action"]] = payoff
 
         # Write agent's move
-            
-	# Write summary statistics for round
+        Agents[i]["lastMove"] = agentAction["move"]
+        Agents[i]["history"]["historyRounds"][] = generation
+        Agents[i]["history"]["historyMoves"][] = agentAction["move"]
+        Agents[i]["history"]["historyActs"][] = agentAction["action"]
+        Agents[i]["history"]["historyPayoffs"][] = payoff
+        Agents[i]["history"]["historyDemes"][] = Agents[i]["currentDeme"]
 
-# Kill N random agents
-            
+        # Write lifespan info
+        gameStats[generation][Agents[i]["strategy"]]["lifespans"][] = Agents[i]["roundsAlive"]        
 
-	# Choose N individuals to reproduce based on current relative fitness
+    # Kill N random agents
+    aliveAgents.shuffle()
+    aliveAgents.slice(-Math.floor(pdie*len(AliveAgents))) # CHECK SYNTAX
 
-	# Randomly mutate x% of N new births
+    # Choose N individuals to reproduce based on current relative fitness
+    for i in aliveAgents:
+        if random.random() < Agents[i]["total"] / Agents[i]["roundsAlive"]:
+            # reproduce
 
-	# Write statistics for reproduction
+    # Randomly mutate x% of N new births
 
-	# Change environment
+    # Write statistics for reproduction
 
-        # Move agents if demes are enabled
+    # Change environment
+
+    # Move agents if demes are enabled
 
 # Write summaries for entire run
